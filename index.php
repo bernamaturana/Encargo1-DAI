0b6bde30087017f8fe85477b560f626db4afcfee
bernamaturana <berna.maturana@gmail.com>
2018-05-12 22:40:17 -0300

    Cambio titulo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <title>aksdjf</title>
+    <title>Lion Vehiculos</title>
 </head>
 <body>
     <a href="consigna.pdf" target="_blank">CONSIGNA</a>
