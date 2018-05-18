53afac98cbbe237fd3928edda7f5baa28279515d
Jancarlos Avello <jota.avello@gmail.com>
2018-05-17 22:43:56 -0400

    Index version 1.0

--- index.php
+++ index.php
@@ -8,14 +8,49 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <title>Lion Vehiculos</title>
     </head>
     <body>
+        <div class="container">
+
+            <div class="row">
+                <h1>Lion Rent a Car</h1>
+            </div>
+            <header class="row justify-content-between center">
+            <div class="columna-nav-3 ">
+                <h4><img src="img/logo-lion2.png" ></h4>
+            </div>
+
+            <div class="columna-nav ">
+                
+                <h4> Inicio</h4>
+                
+                
+            </div>
+            <div class="columna-nav ">
+                <h4>Arrendar Auto</h4>
+            </div>
+
+            <div class="columna-nav ">
+                <h4>Cobrar Arriendo</h4>
+            </div>
+
+            <div class="columna-nav ">
+                <h4>Mi Perfil</h4>
+            </div>
+
+            <div class="columna-nav ">
+                <h4>Cerrar Sesion</h4>
+            </div>
+        </header>
         <a href="consigna.pdf" target="_blank">CONSIGNA</a>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni quaerat nemo voluptas nisi recusandae hic tempora voluptates tempore sequi pariatur enim, est distinctio quod vel, quidem odit mollitia consectetur. Voluptas.</p>
 
 
+        </div>
+        
         <script src="js/jquery-3.3.1.min.js"></script>
         <script src="js/popper.min.js"></script>
     </body>
